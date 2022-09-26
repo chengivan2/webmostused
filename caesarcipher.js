7a4cbd8bf5ac77c1097d7d098494f2c630a16f51
chengivan01 <chengoleivan@gmail.com>
2022-09-26 17:42:51 +0300

    Caesar's cipher explained

--- caesarcipher.js
+++ caesarcipher.js
@@ -1,5 +1,17 @@
 //s is the string you want to decrypt.
-//k is the number of shifts to make in the clockwise direction.
+//k is the number of shifts to make to the right.
+/* 
+    In cryptography, a Caesar cipher, also known as Caesar's cipher, 
+    the shift cipher, Caesar's code or Caesar shift, 
+    is one of the simplest and most widely known encryption techniques. 
+    It is a type of substitution cipher in which each letter 
+    in the plaintext is replaced by a letter 
+    some fixed number of positions down the alphabet. 
+    For example, with a left shift of 3, D would be replaced 
+    by A, E would become B, and so on. 
+    The method is named after Julius Caesar, 
+    who used it in his private correspondence.
+*/
 s = 'abcdefghijklmnop';
 k = 5
 function caesarCipher(s, k) {
